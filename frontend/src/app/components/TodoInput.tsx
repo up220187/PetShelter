8d6e4044b936bb919f91a57efef7256b61fcd834
ReynaMartinez <up220187@alumnos.upa.edu.mx>
2025-07-09 16:17:09 -0600

    actualización clase anterior

--- frontend/src/app/components/TodoInput.tsx
+++ frontend/src/app/components/TodoInput.tsx
@@ -10,8 +10,14 @@ export default function TodoInput({}) {
     //const tasks = ['Item 1', 'Item 2', 'Item 3']
 
     const handClick =() => {
-        setCount(count + 1);
-        console.log(':3', inputValue);
+        setTasks([...tasks, inputValue]);  //agrega a el arreglo en valor del input
+        setInputValue('');                  // limpia el input
+
+    };
+
+    const handleDelete = (index: number) => {
+        const newTasks = tasks.filter((_, i) => i !== index); //filtra las tareas para eliminar la tarea seleccionada
+        setTasks(newTasks); //actualiza el estado de las tareas
     };
 
     return(
@@ -23,10 +29,13 @@ export default function TodoInput({}) {
             onChange={(e) => setInputValue(e.target.value)}/>
             <button onClick={handClick}>Agregar</button>
             <label> {count} </label>
+
+            <p>tareas guardadas:</p>
             <ul>
             {tasks.map((task,index) => (
             <li>
                 {task}
+                <button onClick={() => handleDelete(index)} >Eliminar</button>    
             </li>
             ))}
  
--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,3 +1,27 @@
+'use client'
+
+import {useState} from 'react';
+
 export default function Page() {
-    return <p>Dashboard Page</p>
+    const [visible, setVisible] = useState(false);
+
+    const toggle = () => {
+        setVisible(!visible);
+    };
+
+
+    return (
+        <div style={{ padding: '20px' , textAlign: 'center'}}>
+            <h1>Mostrar / ocultar </h1>
+            <button onClick={toggle}>
+                {visible ? 'Ocultar' : 'Mostrar'}
+            </button>
+
+            {visible && <p>Este mensaje esta oculto</p> }
+        
+        </div>
+
+
+
+    )
 }
