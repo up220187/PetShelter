9ebf819a6ebdbe17687f9865c94155e519ddde8a
UP220786 <Up220786@alumnos.upa.edu.mx>
2025-07-11 00:52:01 -0600

    Cambios de la clase 10 de julio

--- frontend/src/app/components/TodoInput.tsx
+++ frontend/src/app/components/TodoInput.tsx
@@ -1,6 +1,9 @@
 'use client'
 
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:4000/tasks'
 
 export default function TodoInput({}) {
     const [count, setCount] = useState(0);
@@ -8,16 +11,37 @@ export default function TodoInput({}) {
     const [inputValue, setInputValue] = useState('');
 
     //const tasks = ['Item 1', 'Item 2', 'Item 3']
+    // Promesa
+    useEffect(() => {
+        axios.get(API_URL)
+        .then(res => setTasks(res.data))
+        .catch(err => console.log('Error al cargar tareas:', err));
+    },[]);
 
-    const handClick =() => {
-        setTasks([...tasks, inputValue]);  //agrega a el arreglo en valor del input
-        setInputValue('');                  // limpia el input
+    const handClick = async() => {
+
+        try {
+            const res = await axios.post(API_URL, {task: inputValue});
+            setTasks(res.data.tasks); //actualiza lista con respuesta del backend
+            setInputValue(''); //limpia el input
+        } catch (err){
+            console.log('Error al agregar tarea:', err);
+        }
+
+        //setTasks([...tasks, inputValue]);  //agrega a el arreglo en valor del input
+        //setInputValue('');                  // limpia el input
 
     };
 
-    const handleDelete = (index: number) => {
-        const newTasks = tasks.filter((_, i) => i !== index); //filtra las tareas para eliminar la tarea seleccionada
-        setTasks(newTasks); //actualiza el estado de las tareas
+    const handleDelete = async(index: number) => {
+        try{
+            const res = await axios.delete(`${API_URL}/${index}`);
+            setTasks(res.data.tasks); //actualiza lista con respuesta del backend
+        }catch (err){  
+            console.log('Error al agregar tarea:', err);
+        }
+        //const newTasks = tasks.filter((_, i) => i !== index); //filtra las tareas para eliminar la tarea seleccionada
+        //setTasks(newTasks); //actualiza el estado de las tareas
     };
 
     return(
