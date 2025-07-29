e8930f85158cdef44f9d8134c172ed4717aec3e7
Anahi Varela <up220332@alumnos.upa.edu.mx>
2025-07-29 17:46:54 -0600

    Register terminado

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,35 +1,22 @@
 "use client";
 import RegisterForm from "../components/RegisterForm";
-import VerMascotasButton from "../components/VerMascotasButton";
-
 
 export default function RegisterPage() {
   return (
-    <div className="min-h-screen bg-white relative overflow-hidden">
-      {/* Header hasta arriba que cubre todo */}
-      
-      
-      {/* Contenido principal */}
-      <div className="flex flex-col items-center justify-center pt-8">
-        {/* Logo centrado */}
-        <div className="text-center mb-8 z-10">
-          <img 
+    <div style={{ position: 'relative', overflow: 'hidden' }}>
+
+      <div className="global-paws-background"></div>
+
+      <div className="main-content-wrapper">
+
+        <div className="logo-container">
+          <img
             src="/logo1.png" 
-            alt="Pet Shelter Logo" 
-            className="w-64 h-auto mx-auto"
+            alt="Pet Shelter Logo"
           />
         </div>
 
-        {/* Imagen de huellas decorativas al lado del formulario */}
-        <img
-          src="/paws.png"
-          alt="Huellas"
-          className="absolute right-0 top-32 w-64 h-auto z-0"
-        />
-
-        {/* Formulario centrado */}
-        <div className="relative z-10">
-          <VerMascotasButton/>
+        <div className="form-box">
           <RegisterForm />
         </div>
       </div>
